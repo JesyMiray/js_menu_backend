53e90575424b4fa2ef82c950986ed6cddde8bdc4
torch <aleksandrow.dima8@gmail.com>
2024-05-15 22:57:44 +0300

    changing the server part under the menu

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {MongoClient, ObjectId} = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 const app = express();
 const port = 8000;
@@ -11,40 +11,63 @@ async function getDbCollection(adAddress, dbName, dbCollectionName) {
 	const client = new MongoClient(adAddress);
 	await client.connect();
 	const db = client.db(dbName);
-	return collection = db.collection(dbCollectionName);
+	return db.collection(dbCollectionName);
 }
 
-app.get("/tasks", async function(req, res) {
-	const collection = await getDbCollection("mongodb://127.0.0.1","todoapp", "task");
-	const data = await collection.find({}).toArray();
-	res.send(data);
+//get all menu categories with dishes
+app.get("/menu", async function(req, res) {
+	const categoriesCollection = await getDbCollection("mongodb://127.0.0.1", "restaurantapp", "categories");
+	const dishesCollection = await getDbCollection("mongodb://127.0.0.1", "restaurantapp", "dishes");
+
+	const categories = await categoriesCollection.find({}).toArray();
+	const dishes = await dishesCollection.find({}).toArray();
+
+	const menu = categories.map(category => ({
+		...category,
+		dishes: dishes.filter(dish => dish.categoryId.toString() === category._id.toString())
+	}));
+
+	res.send(menu);
 });
 
-app.get("/tasks/:id", async function(req, res) {
-	const collection = await getDbCollection("mongodb://127.0.0.1","todoapp", "task");
-	const data = await collection.findOne({_id: new ObjectId(req.params.id)});
-	res.send(data);
+//add a new category
+app.post("/categories", async function(req, res) {
+	const category = { name: req.body.name };
+	const collection = await getDbCollection("mongodb://127.0.0.1", "restaurantapp", "categories");
+	const result = await collection.insertOne(category);
+	res.send({ ...category, id: result.insertedId });
 });
 
-app.post("/tasks",async function(req, res) {
-	const task = {...req.body, done: false};
-	const collection = await getDbCollection("mongodb://127.0.0.1","todoapp", "task");
-	await collection.insertOne(task);
-	res.send(task);
+//add a new dish
+app.post("/dishes", async function(req, res) {
+	const dish = {
+		categoryId: new ObjectId(req.body.categoryId),
+		name: req.body.name,
+		grams: req.body.grams,
+		price: req.body.price
+	};
+	const collection = await getDbCollection("mongodb://127.0.0.1", "restaurantapp", "dishes");
+	const result = await collection.insertOne(dish);
+	res.send({ ...dish, id: result.insertedId });
 });
 
-app.patch("/tasks/:id", async function(req, res) {
-	const collection = await getDbCollection("mongodb://127.0.0.1","todoapp", "task");
-	const data = await collection.updateOne({_id: new ObjectId(req.params.id)},{"$set": req.body});
-	res.send(data);
+//update a dish
+app.patch("/dishes/:id", async function(req, res) {
+	const collection = await getDbCollection("mongodb://127.0.0.1", "restaurantapp", "dishes");
+	const result = await collection.updateOne(
+		{ _id: new ObjectId(req.params.id) },
+		{ $set: req.body }
+	);
+	res.send(result);
 });
 
-app.delete("/tasks/:id", async function(req, res) {
-	const collection = await getDbCollection("mongodb://127.0.0.1","todoapp", "task");
-	await collection.deleteOne({_id: new ObjectId(req.params.id)});
-	res.send({});
+//delete a dish
+app.delete("/dishes/:id", async function(req, res) {
+	const collection = await getDbCollection("mongodb://127.0.0.1", "restaurantapp", "dishes");
+	const result = await collection.deleteOne({ _id: new ObjectId(req.params.id) });
+	res.send(result);
 });
 
 app.listen(port, function() {
-	console.log("server started!");
+	console.log(`Server started on port ${port}!`);
 });
